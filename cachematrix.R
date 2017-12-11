7951d00b46b8ccd4e5087f23f5c5ecf18b65a29c
mulderc <cameron.mulder@gmail.com>
2017-12-10 22:01:56 -0800

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,11 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function takes a matrix x, assumed to be invertable, and creates an object 
+## which has the data supplied by X, a null data object m, and 4 functions 
+## set, get, setsolve, getsolve
+## this object is justed by cacheSolve to calculate the inverse of the matrix and 
+## then save the results so we don't have to caluculate it again
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -19,6 +24,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## this function takes the object create by the makeCacheMatrix()
+## sees if the inverted X matrix has been solved
+## If it has then it returns the already solved inverted matrix
+## if it has not already been solved then it calculates the inverse of the matrix X
 
 cacheSolve <- function(x, ...) {
   m <- x$getsolve()
